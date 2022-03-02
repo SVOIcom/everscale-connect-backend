d3a7d270dd5fddfb3dd9567fcb8f130924f536e6
Andrey Nedobylsky <admin@twister-vl.ru>
2022-03-02 11:36:59 +0100

    Test inject EVER module as TON

--- public/everscale/0.1.1/providers/EverWeb/Contract.mjs
+++ public/everscale/0.1.1/providers/EverWeb/Contract.mjs
@@ -85,6 +85,42 @@ class Contract {
         return balance;
     }
 
+    async _runLocal(abi, address, functionName, input = {}) {
+        let TON = this.ton.lowLevel;
+
+        const account = (await TON.net.query_collection({
+            collection: 'accounts',
+            filter: {id: {eq: address}},
+            result: 'boc'
+        })).result[0].boc;
+
+        const message = await TON.abi.encode_message({
+            abi: {
+                type: 'Contract',
+                value: (abi)
+            },
+            address: address,
+            call_set: {
+                function_name: functionName,
+                input: input
+            },
+            signer: {
+                type: 'None'
+            }
+        });
+
+        let response = await TON.tvm.run_tvm({
+            message: message.message,
+            account: account,
+            abi: {
+                type: 'Contract',
+                value: (abi)
+            },
+        });
+
+        return response.decoded.output;
+    }
+
     /**
      * Run method locally
      * @param {string} method
@@ -100,6 +136,8 @@ class Contract {
         })).output;
         //return await this.contract.functions[method].runGet(args);
     }
+
+
 
     /**
      * Deploy method
