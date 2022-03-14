407c169a0f81016d3075cc726b740366947f228c
Andrey Nedobylsky <admin@twister-vl.ru>
2022-03-14 14:06:51 +0100

    jQuery update test

--- public/everscale/0.1.1/providers/jQuery.mjs
+++ public/everscale/0.1.1/providers/jQuery.mjs
@@ -1,3 +1,8 @@
-import 'https://code.jquery.com/jquery-3.6.0.min.js'
-const jQuery = window.jQuery.noConflict(true);
+let jQuery = window.jQuery;
+if(!window.jQuery) {
+
+    await import('https://code.jquery.com/jquery-3.6.0.min.js');
+
+    jQuery = window.jQuery;//.noConflict(true);
+}
 export default jQuery;
